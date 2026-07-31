b738b4a9cbee0ddeb2071d792a9ad7bd646d7f40
Benjamin <benjamin@fcverachtert.be>
2026-07-31 14:46:53 +0200

    Fix: Remove duplicate closing tags in Statistics

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -163,10 +163,6 @@ export default function Statistics() {
                     </tr>
                   ))}
                 </tbody>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
               </table>
             </div>
           ) : (
